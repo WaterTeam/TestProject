972a832d9958c2b6c5c794c412496c2e20aff055
CNTcnt <CNTcnt@gmail.com>
2017-12-05 11:34:22 +0800

    第二次提交添加了方法

--- app/src/main/java/com/example/ba/testgithub/AddClassByCNT.java
+++ app/src/main/java/com/example/ba/testgithub/AddClassByCNT.java
@@ -9,4 +9,12 @@ public class AddClassByCNT {
     //往CNT代码中测试
     public void BASet(){
     }
+/**
+ *添加方法
+ * @author CNT on 2017/12/5.
+ * @param
+ * @return
+ * @exception
+ */
+    void test(){}
 }
